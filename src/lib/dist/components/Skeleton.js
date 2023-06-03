f408e0004288a695a0ce269ef1f31be28c217a65
Moussa Alkaly BADJI <moussabadji079@gmail.com>
2023-06-03 18:29:50 +0000

    fix error in skeloton.tsx instead of skeleton.jsx

---
+++ src/lib/dist/components/Skeleton.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _react = _interopRequireDefault(require("react"));
+var _jsxRuntime = require("react/jsx-runtime");
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+const Skeleton = () => {
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+    children: "Skeleton"
+  });
+};
+var _default = Skeleton;
+exports.default = _default;
