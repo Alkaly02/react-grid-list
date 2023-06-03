0ac38918c0c58f0516eb2cf141203f0360b17bb8
Moussa Alkaly BADJI <moussabadji079@gmail.com>
2023-06-03 18:43:20 +0000

    feat: add style to skeleton components

--- src/lib/dist/components/Skeleton.js
+++ src/lib/dist/components/Skeleton.js
@@ -5,11 +5,27 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 var _react = _interopRequireDefault(require("react"));
+require("./skeleton.css");
 var _jsxRuntime = require("react/jsx-runtime");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 const Skeleton = () => {
-  return /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
-    children: "Skeleton"
+  return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+    className: "movie--isloading",
+    children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+      className: "loading-image"
+    }), /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+      className: "loading-content",
+      children: /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+        className: "loading-text-container",
+        children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+          className: "loading-main-text"
+        }), /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+          className: "loading-main-text"
+        }), /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+          className: "loading-sub-text"
+        })]
+      })
+    })]
   });
 };
 var _default = Skeleton;
--- src/lib/package/components/Skeleton.tsx
+++ src/lib/package/components/Skeleton.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './skeleton.css'
 
 const Skeleton = () => {
   return (
-    <div>Skeleton</div>
+    <div className="movie--isloading">
+      <div className="loading-image"></div>
+      <div className="loading-content">
+        <div className="loading-text-container">
+          <div className="loading-main-text"></div>
+          <div className="loading-main-text"></div>
+          <div className="loading-sub-text"></div>
+        </div>
+      </div>
+      {/* <div className="loading-btn"></div> */}
+    </div>
   )
 }
 
